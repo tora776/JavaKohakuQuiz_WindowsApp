9e6a210a15fbb80675e1ac8663713e195b3f37b6
openi <openi@Nakaoka-Desk>
2025-01-09 12:01:22 +0900

    20250109_クイズテンプレート編集中

--- KohakuQuiz/src/main/java/com/quiz/kohaku/controller/IndexController.java
+++ KohakuQuiz/src/main/java/com/quiz/kohaku/controller/IndexController.java
@@ -16,18 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
-	/*
-	private final ArtistService artistService;
-	private final HostService hostService;
-	private final ResultService resultService;
-	    
-    @Autowired
-    public IndexController(ArtistService artistService, HostService hostService, ResultService resultService) {
-        this.artistService = artistService;
-        this.hostService = hostService;
-        this.resultService = resultService;
-    }
-    */
 	
 	// SpringのコンテナからUtilインスタンスを注入する
 	@Autowired
@@ -39,11 +27,6 @@ public class IndexController {
         // 10個のクイズを作成
         List<Quiz> quizList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-        	/*
-            Quiz quiz = new Quiz();
-            quiz.setQuiz("年の紅白歌合戦の総合司会は誰？");
-            quizList.add(quiz);
-            */
         	// クイズを作成
         	Quiz quiz = util.GenerateQuizzes();
         	quizList.add(quiz);
@@ -57,43 +40,11 @@ public class IndexController {
         return "index";
     }
 		
-		
 	
 	@PostMapping("/submit")
 	public String submitForm(@ModelAttribute Form form, Model model) {
 		model.addAttribute("answers", form.getAnswers());
 		return "result";
 	}
-	/*
-	@GetMapping("/artists")
-	public String showArtists(Model model) {
-	    // アーティストデータを取得してモデルに追加
-	    List<Artist> artists = artistService.artistList();
-	    model.addAttribute("artists", artists);
-
-	    // artist.htmlを表示
-	    return "artist";
-	}
-	
-	@GetMapping("/hosts")
-	public String showHosts(Model model) {
-	    // ホストデータを取得してモデルに追加
-	    List<Host> hosts = hostService.hostList();
-	    model.addAttribute("hosts", hosts);
-
-	    // host.htmlを表示
-	    return "host";
-	}
-	
-	@GetMapping("/resultKohaku")
-	public String showResults(Model model) {
-	    // 勝敗データを取得してモデルに追加
-	    List<Result> results = resultService.resultList();
-	    model.addAttribute("results", results);
-
-	    // result.htmlを表示
-	    return "resultKohaku";
-	}
-	*/
 	
 }
--- KohakuQuiz/src/main/java/com/quiz/kohaku/util/Util.java
+++ KohakuQuiz/src/main/java/com/quiz/kohaku/util/Util.java
@@ -19,7 +19,6 @@ import com.quiz.kohaku.service.ResultService;
 public class Util {
 	
 	private final ArtistService artistService;
-
 	private final HostService hostService;
 	private final ResultService resultService;
 	private final QuizService quizService;
@@ -33,23 +32,139 @@ public class Util {
     
 	
 	public Quiz GenerateQuizzes() {
+		List<Quiz> quizzes = quizService.quizList();
+		// ランダムな数値を取得
+		Random random = new Random();
+		int quiz_id = random.nextInt(quizzes.size());
+		// クイズを作成
+		Quiz quiz = new Quiz();
+		String quiz_template = quizzes.get(quiz_id).getQuiz_template();
+		quiz.setQuiz(quiz_template);
+		// 選択肢を作成
+		
+		return quiz;
+	}
+	
+	private Quiz buildQuizFromTemplate(int quiz_id, List<Quiz> quizzes) {
 		// DBよりリストを取得
 		List<Artist> artists = artistService.artistList();
 		List<Host> hosts = hostService.hostList();
 		List<Result> results = resultService.resultList();
-		List<Quiz> quizzes = quizService.quizList();
-		// ランダムな数値を取得
-		Random random = new Random();
-		int randomArtistName = random.nextInt(artists.size());
+		// クイズを取得
+		Quiz quiz = quizzes.get(quiz_id);
+		// クイズのテンプレートを取得
+		String quiz_template = quiz.getQuiz_template();
+		String choice_template1 = quiz.getChoice_template1();
+		String choice_template2 = quiz.getChoice_template2();
+		String choice_template3 = quiz.getChoice_template3();
+		String choice_template4 = quiz.getChoice_template4();
 		
-		Random random2 = new Random();
-		int randomQuizNumber = random2.nextInt(quizzes.size());
-		// クイズを作成
-		// Quiz quiz = new Quiz();
-		// quiz.setQuiz(artists.get(randomArtistName).getArtist_name() + "は2024年に出場している？");
-		Quiz quiz = new Quiz();
-		String quiz_template = quizzes.get(randomQuizNumber).getQuiz_template();
-		quiz.setQuiz(quiz_template);
+		// テンプレートに値を代入
+		switch(quiz_id) {
+			case 1: // 2025年現在、{アーティスト名}は過去{x}回紅白に出場している。
+				// アーティスト名・出場回数を取得
+				Random random = new Random();
+				int artist_id = random.nextInt(artists.size());
+				Artist artist = artists.get(artist_id);
+				String artist_name = artist.getArtist_name();
+				String appearance = String.valueOf(artist.getAppearance());
+				// テンプレートを置き換え
+				quiz_template.replace("{アーティスト名}", artist_name);
+				quiz_template.replace("{x}", appearance);
+				// クイズを作成
+				quiz.setQuiz(quiz_template);
+				break;
+			case 2: // 2025年現在、{アーティスト名}は{アーティスト名}より紅白の出場回数が多い。
+				
+				break;
+			case 3: // {アーティスト名}が紅白で初めて歌った楽曲は何？
+				
+				break;
+			case 4: // {アーティスト名}が紅白出場{x}回目で歌った楽曲は何？
+				
+				break;
+			case 5: // {アーティスト名}は{曲名}を{x}回歌っている？
+				
+				break;
+			case 6: // {yyyy}年の紅白歌合戦の総合司会は誰？
+				
+				break;
+				
+			case 7: // {yyyy}年の紅白歌合戦の紅組司会は誰？
+				
+				break;
+			case 8: // {yyyy}年の紅白歌合戦の白組司会は誰？
+				
+				break;
+			case 9: // {yyyy}年の紅白歌合戦で司会を務めたのは、a,bとあと一人は？
+				
+				break;
+			case 10: // 2025年現在、歴代紅白歌合戦でどちらのほうが勝利回数が多い？
+				
+				break;
+			case 11: // 昨年{yyyy年}の紅白歌合戦の勝利チームは？
+				
+				break;
+			default:
+		}
+		return quiz;
+	}
+	
+	private Quiz buildChoiceFromTemplate(int quiz_id, List<Quiz> quizzes) {
+		// DBよりリストを取得
+		List<Artist> artists = artistService.artistList();
+		List<Host> hosts = hostService.hostList();
+		List<Result> results = resultService.resultList();
+		// クイズを取得
+		Quiz quiz = quizzes.get(quiz_id);
+		// テンプレートに値を代入
+			switch(quiz_id) {
+				case 1: // 2025年現在、{アーティスト名}は過去{x}回紅白に出場している。
+					// アーティスト名・出場回数を取得
+					Random random = new Random();
+					int artist_id = random.nextInt(artists.size());
+					Artist artist = artists.get(artist_id);
+					String artist_name = artist.getArtist_name();
+					String appearance = String.valueOf(artist.getAppearance());
+					// 選択肢を置き換え
+					
+					// クイズを作成
+					
+					break;
+				case 2: // 2025年現在、{アーティスト名}は{アーティスト名}より紅白の出場回数が多い。
+					
+					break;
+				case 3: // {アーティスト名}が紅白で初めて歌った楽曲は何？
+					
+					break;
+				case 4: // {アーティスト名}が紅白出場{x}回目で歌った楽曲は何？
+					
+					break;
+				case 5: // {アーティスト名}は{曲名}を{x}回歌っている？
+					
+					break;
+				case 6: // {yyyy}年の紅白歌合戦の総合司会は誰？
+					
+					break;
+					
+				case 7: // {yyyy}年の紅白歌合戦の紅組司会は誰？
+					
+					break;
+				case 8: // {yyyy}年の紅白歌合戦の白組司会は誰？
+					
+					break;
+				case 9: // {yyyy}年の紅白歌合戦で司会を務めたのは、a,bとあと一人は？
+					
+					break;
+				case 10: // 2025年現在、歴代紅白歌合戦でどちらのほうが勝利回数が多い？
+					
+					break;
+				case 11: // 昨年{yyyy年}の紅白歌合戦の勝利チームは？
+					
+					break;
+				default:
+			}
+		
 		return quiz;
 	}
 }
