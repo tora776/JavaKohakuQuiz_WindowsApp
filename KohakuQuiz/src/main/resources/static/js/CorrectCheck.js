48eb5361253a21ad2712c18548098a0d361c35d6
openi <openi@Nakaoka-Desk>
2025-01-14 16:21:28 +0900

    20250114_css「×」マーク表示可能

--- KohakuQuiz/src/main/resources/static/js/CorrectCheck.js
+++ KohakuQuiz/src/main/resources/static/js/CorrectCheck.js
@@ -4,23 +4,46 @@
 'use strict'
 function correctCheck(corrections){
 	var table = document.getElementById("table");
-	var tr = table.tr.rows;
+	var tr = table.tBodies[0].rows;
 	
 	
 	for(let i = 0; i < corrections.length; i++){
 		if(corrections[i] === true){
-			tr[i].style.background = "blue";
+			// img_element = createCorrectImage();
+			tr[i].style.background = "#F0B9B9";
+			// tr[i].cells[1].appendChild(img_element);
 			console.log("true");
 		} else {
-			tr[i].style.background = "red";
+			var img_element = createInCorrectImage();
+			tr[i].style.background = "#D3DEF1";
+			tr[i].cells[1].appendChild(img_element);
 			console.log("false")
 		}
 	}
 }
 
+
+
+function createInCorrectImage(){
+	let img_element = document.createElement('img');
+	img_element.src = '../img/incorrect.png';
+	img_element.alt = '不正解';
+	img_element.width = 100;
+	img_element.height = 100;
+	img_element.classList.add("img");
+	// source要素を追加
+	// let source_element = document.createElement('source');
+	// source_element.srcset = '..img/incorrect.png';
+	// source_element.media = '(min-width: 800px)';
+
+	// picture要素にimg要素とsource要素を追加
+	// picture_element.appendChild(img_element);
+	// picture_element.appendChild(source_element);
+	return img_element;
+}
+
 // ページ読み込み後に実行
 document.addEventListener('DOMContentLoaded', () => {
-    const corrections = document.getElementById('correctionsData');
-    console.log(corrections);
+    const corrections = JSON.parse(correctionsData.textContent);
 	correctCheck(corrections);
 });
